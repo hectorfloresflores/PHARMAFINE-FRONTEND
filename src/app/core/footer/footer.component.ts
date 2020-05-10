d9b52451340c73d18d57b7de6b1ce625788b8d1c
RaulMendezA <rulax2313@gmail.com>
2020-05-09 23:53:49 -0500

    Modal working

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  role = localStorage.getItem('userRole');
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
