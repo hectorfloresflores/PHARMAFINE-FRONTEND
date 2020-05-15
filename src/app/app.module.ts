4adad0c85138c903b375d3ebb7fa2f9f234cadc6
RaulMendezA <rulax2313@gmail.com>
2020-05-14 21:15:47 -0500

    Cloudinary service uploads working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,33 @@ import { NgModule } from '@angular/core';
 
 
 
-import { AppRoutingModule, routingComponents} from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
-import { SocketIoModule, SocketIoConfig} from 'ngx-socket-io';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { HomeComponent } from './modules/home/pages/home/home.component';
 import { ProductsComponent } from './modules/products/pages/products/products.component';
 import { HeaderComponent } from './core/header/header.component';
 import { SigninComponent } from './core/header/signin/signin.component';
 import { RegisterComponent } from './core/header/register/register.component';
-import {ReactiveFormsModule, FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {AuthenticationService} from './core/authentication/authentication.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationService } from './core/authentication/authentication.service';
 import { UserComponent } from './modules/user/pages/user/user.component';
 import { AdminComponent } from './modules/admin/pages/admin/admin.component';
 import { UserChatComponent } from './modules/user/pages/user-chat/user-chat.component';
 import { AdminChatComponent } from './modules/admin/pages/admin-chat/admin-chat.component';
 import { environment } from 'src/environments/environment.prod';
-import {ProductListComponent} from "./modules/products/components/product-list/product-list.component";
+import { ProductListComponent } from './modules/products/components/product-list/product-list.component';
+import { FileSelectDirective } from 'ng2-file-upload';
+import { CloudinaryModule } from '@cloudinary/angular-5.x';
+import * as  Cloudinary from 'cloudinary-core';
 // This app was made it with the folder structure expose in the link below...
 // https://itnext.io/choosing-a-highly-scalable-folder-structure-in-angular-d987de65ec7
-const config: SocketIoConfig = { url: environment.url , options: {}};
+const config: SocketIoConfig = { url: environment.url, options: {} };
 
 @NgModule({
   declarations: [
@@ -42,16 +45,22 @@ const config: SocketIoConfig = { url: environment.url , options: {}};
     UserChatComponent,
     AdminChatComponent,
     ProductListComponent,
+    FileSelectDirective
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NgbModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        FormsModule,
-        SocketIoModule.forRoot(config)
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SocketIoModule.forRoot(config),
+    CloudinaryModule.forRoot(Cloudinary, {
+      cloud_name: 'pae2020',
+      api_key: '865312224523489',
+      api_secret: 'teTUMLdTOvYgC0rCGZOGkfbuppg'
+    })
+  ],
   providers: [AuthenticationService],
   bootstrap: [AppComponent]
 
--- src/app/modules/user/pages/user/user.component.ts
+++ src/app/modules/user/pages/user/user.component.ts
@@ -7,12 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   user;
+  edited = false;
+  userPic = "";
+  private widget: any = null;
+
 
   constructor() { }
 
   ngOnInit(): void {
     if (localStorage.user != 'undefined' && localStorage.user != undefined)
-    this.user = JSON.parse(localStorage.user); //para obter user local
+      this.user = JSON.parse(localStorage.user); //para obter user local
+
+    this.user.url = "https://res.cloudinary.com/pae2020/image/upload/v1589502064/mihgrswk3ad6aybtdbd3.jpg"; // iría user.url (directo de localStorage)
+
+    this.widget = (window as any).cloudinary.createUploadWidget(
+      {
+        cloudName: 'pae2020',
+        uploadPreset: 'ubmahvyp',
+        showPoweredBy: false,
+        clientAllowedFormats: ["png","gif", "jpeg"],
+        theme: 'minimal',
+        cropping: true,
+        croppingAspectRatio: 1.0,
+        croppingDefaultSelectionRatio: 1.0
+      },
+      (error, result) => {
+        if (!error && result && result.event === 'success') {
+          console.log('Subida! secure url: ', result.info.secure_url);
+          this.userPic = result.info.secure_url;
+          this.edited = true;
+
+          // hacer post a db aquí, user.url = result.info.secure_url; con Fetch o servicio users
+        }
+      }
+    );
+  }
+
+  onOpenUpload($event) {
+    this.widget.open();
+    console.log('Widget used', $event);
   }
 
 }
