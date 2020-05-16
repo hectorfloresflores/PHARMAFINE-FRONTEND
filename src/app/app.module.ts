a2755a599f7ee43ea4f0c02c03548758f3191cad
hectorfloresflores <48966777+hectorfloresflores@users.noreply.github.com>
2020-05-16 13:16:58 -0500

    v19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,14 @@ import { UserChatComponent } from './modules/user/pages/user-chat/user-chat.comp
 import { AdminChatComponent } from './modules/admin/pages/admin-chat/admin-chat.component';
 import { environment } from 'src/environments/environment.prod';
 import {ProductListComponent} from "./modules/products/components/product-list/product-list.component";
+
+// import { CheckoutComponent } from './modules/checkout/pages/checkout/checkout.component';
+// import { CheckoutListComponent } from './modules/checkout/components/checkout-list/checkout-list.component';
+
+import { FileSelectDirective } from 'ng2-file-upload';
+import { CloudinaryModule } from '@cloudinary/angular-5.x';
+import * as  Cloudinary from 'cloudinary-core';
+
 // This app was made it with the folder structure expose in the link below...
 // https://itnext.io/choosing-a-highly-scalable-folder-structure-in-angular-d987de65ec7
 const config: SocketIoConfig = { url: environment.url , options: {}};
@@ -42,6 +50,24 @@ const config: SocketIoConfig = { url: environment.url , options: {}};
     UserChatComponent,
     AdminChatComponent,
     ProductListComponent,
+    // CheckoutComponent,
+    // CheckoutListComponent,
+    FileSelectDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SocketIoModule.forRoot(config),
+    CloudinaryModule.forRoot(Cloudinary, {
+      cloud_name: 'pae2020',
+      api_key: '865312224523489',
+      api_secret: 'teTUMLdTOvYgC0rCGZOGkfbuppg'
+    })
+
   ],
     imports: [
         BrowserModule,
